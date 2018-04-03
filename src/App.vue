a342d2dddb6795dd9500d0cbbdb1502610e1a0fe
xiaoconghu <huxc10086@163.com>
2018-04-03 10:09:11 +0800

    增加用户按钮

--- src/App.vue
+++ src/App.vue
@@ -1,129 +1,157 @@
 <template>
-  <div id="app">
-    <h3>Chicken Music</h3>
-    <mt-navbar v-model="selected" class="nav-bar">
-      <mt-tab-item id="1" class="nav-item">
-        <router-link class="nav-item-link" to="/recommend">推荐</router-link>
-      </mt-tab-item>
-      <mt-tab-item id="2" class="nav-item">
-        <router-link class="nav-item-link" to="/singer">歌手</router-link>
-      </mt-tab-item>
-      <mt-tab-item id="3" class="nav-item">
-        <router-link class="nav-item-link" to="/seniority">排行</router-link>
-      </mt-tab-item>
-      <mt-tab-item id="4" class="nav-item">
-        <router-link class="nav-item-link" to="/search">搜索</router-link>
-      </mt-tab-item>
-    </mt-navbar>
-
-    <!-- tab-container -->
-    <router-view></router-view>
-    <player></player>
-  </div>
+    <div id="app">
+        <header class="header">
+            <h3>Chicken Music</h3>
+            <span class="user" @click="$router.push({ path: 'user'})">
+                <i class="iconfont icon-yonghu"></i>
+            </span>
+        </header>
+        <mt-navbar v-model="selected" class="nav-bar">
+            <mt-tab-item id="1" class="nav-item">
+                <router-link class="nav-item-link" to="/recommend">推荐</router-link>
+            </mt-tab-item>
+            <mt-tab-item id="2" class="nav-item">
+                <router-link class="nav-item-link" to="/singer">歌手</router-link>
+            </mt-tab-item>
+            <mt-tab-item id="3" class="nav-item">
+                <router-link class="nav-item-link" to="/seniority">排行</router-link>
+            </mt-tab-item>
+            <mt-tab-item id="4" class="nav-item">
+                <router-link class="nav-item-link" to="/search">搜索</router-link>
+            </mt-tab-item>
+        </mt-navbar>
+        <router-view></router-view>
+        <player></player>
+    </div>
 </template>
 
 <script>
-import './assets/iconfont/iconfont.css'
-import player from './components/player/player'
-import './assets/iconfont_2/iconfont.css'
+  import './assets/iconfont/iconfont.css'
+  import player from './components/player/player'
+  import './assets/iconfont_2/iconfont.css'
 
-export default {
-  name: 'App',
-  components: {
-    player
-  },
-  data(){
-    return {
-      activeIndex2: '1',
-      selected:'1',
-    }
-  },
-  methods: {
-    handleSelect(key, keyPath) {
-      console.log(key, keyPath);
-    }
-  }
-}
-</script>
-
-<style lang="scss"  type="text/scss">
-  body{
-    background: #222;
-  }
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  background: #222;
-  h3{
-    margin: 0;
-    height: 44px;
-    line-height: 44px;
-    text-align: center;
-    color: #ffcd32;
-  }
-    .mint-navbar .mint-tab-item.is-selected {
-        border-bottom: none;
-        margin-bottom: -3px;
-    }
-  .nav-bar{
-    background: #222;
-    .nav-item{
-        .router-link-active{
-            border-bottom: 2px solid #26a2ff;
-            color: #26a2ff;
-            margin-bottom: 3px;
-        }
-      padding: 0;
-      .nav-item-link{
-        color: #ffcd32;
-        display: block;
-        padding: 17px 0;
-        text-decoration: none;
+  export default {
+    name: 'App',
+    components: {
+      player
+    },
+    data() {
+      return {
+        activeIndex2: '1',
+        selected: '1',
+      }
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
       }
     }
   }
-}
-  .mint-indexsection-index {
-    margin: 0;
-    padding: 10px;
-    background: #222222;
-  }
-  .mint-indexlist-nav{
-    border-radius: 10px;
-    color: #fafafa;
-    text-align: center;
-    background: rgba(0,0,0,.3);
-    font-family: Helvetica;
-    border: none;
-  }
-  .mint-indexsection-index{
-    /*height: 30px;*/
-    /*line-height: 30px;*/
-    padding-left: 20px;
-    font-size: 12px;
-    color: hsla(0,0%,100%,.5);
-    background: #333;
-  }
+</script>
 
-  .mint-searchbar{
-    flex: 1;
-    margin: 0 5px;
-    line-height: 18px;
-    background: #333;
-    color: #fff;
-    font-size: 14px;
-    outline: 0;
-    z-index: 1;
-    position: static;
-  }
-  .mintui-search:before {
-    content: "\E604";
-  }
-  .mintui-search{
-    display: inline-block;
-    font-size: 22px !important;
-    color: #222;
-  }
+<style lang="scss" type="text/scss">
+    body {
+        background: #222;
+    }
+
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        color: #2c3e50;
+        background: #222;
+        header{
+            position: relative;
+            h3 {
+                margin: 0;
+                height: 44px;
+                line-height: 44px;
+                text-align: center;
+                color: #ffcd32;
+            }
+            .user{
+                position: absolute;
+                top: 50%;
+                transform: translateY(-50%);
+                right: 5px;
+            }
+        }
+
+        .mint-navbar .mint-tab-item.is-selected {
+            border-bottom: none;
+            margin-bottom: -3px;
+        }
+        .nav-bar {
+            background: #222;
+            .nav-item {
+                .router-link-active {
+                    border-bottom: 2px solid #26a2ff;
+                    color: #26a2ff;
+                    margin-bottom: 3px;
+                }
+                padding: 0;
+                .nav-item-link {
+                    color: #ffcd32;
+                    display: block;
+                    padding: 17px 0;
+                    text-decoration: none;
+                }
+            }
+        }
+        .user{
+            position: absolute;
+            i{
+                display: block;
+                padding: 12px;
+                font-size: 20px;
+                color: #ffcd32
+            }
+        }
+    }
+
+    .mint-indexsection-index {
+        margin: 0;
+        padding: 10px;
+        background: #222222;
+    }
+
+    .mint-indexlist-nav {
+        border-radius: 10px;
+        color: #fafafa;
+        text-align: center;
+        background: rgba(0, 0, 0, .3);
+        font-family: Helvetica;
+        border: none;
+    }
+
+    .mint-indexsection-index {
+        /*height: 30px;*/
+        /*line-height: 30px;*/
+        padding-left: 20px;
+        font-size: 12px;
+        color: hsla(0, 0%, 100%, .5);
+        background: #333;
+    }
+
+    .mint-searchbar {
+        flex: 1;
+        margin: 0 5px;
+        line-height: 18px;
+        background: #333;
+        color: #fff;
+        font-size: 14px;
+        outline: 0;
+        z-index: 1;
+        position: static;
+    }
+
+    .mintui-search:before {
+        content: "\E604";
+    }
+
+    .mintui-search {
+        display: inline-block;
+        font-size: 22px !important;
+        color: #222;
+    }
 </style>
