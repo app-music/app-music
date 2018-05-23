b62074634e758d4a497488b5eb896dde7e1bb8b9
xiaoconghu <huxc10086@163.com>
2018-05-23 17:59:20 +0800

    登陆权限 管理

--- src/router/index.js
+++ src/router/index.js
@@ -68,13 +68,12 @@ router.beforeEach((to, from, next) => {
     // to: Route: 即将要进入的目标 路由对象
     // from: Route: 当前导航正要离开的路由
     // next: Function: 一定要调用该方法来 resolve 这个钩子。执行效果依赖 next 方法的调用参数。
-    console.log(to);
     let loginInfo =JSON.parse(sessionStorage.getItem('loginInfo'));  // 是否登录
     // 未登录状态；跳转至login
     // 已登录状态；当路由到login时，跳转至recommend
     if (to.name === 'login') {
         if (loginInfo) {
-            next({path:'/recommend'})
+            next({path: from.path})
         }else {
             next();
         }
